bc005e998394deb846d48db8715a1529ce6b1b94
Nikola Rosic <nikolarosic@Nikolas-MacBook-Pro.local>
2022-01-21 22:02:51 +0100

    initial commit

---
+++ teleTrader_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teleTrader_app
+//
+//  Created by Nikola Rosic on 21/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
